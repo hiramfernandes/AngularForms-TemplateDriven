084f714331a461735bcb551642a89cd58cd1274c
Hiram Fernandes <hiram.sd@gmail.com>
2020-04-23 21:19:48 +0100

    Added data service

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    return this.http.post('https://putsreq.com/f5tN6oLxRN2kub7riEiK', userSettings);
+    // return of(userSettings);
+  }
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -19,13 +20,18 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings: UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
     console.log('In onSubmit Event. Valid: ' + form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings)
+      .subscribe(
+        result => console.log('Success: ', result),
+        error => console.log('Error: ', error)
+      );
   }
 
   onBlur(field: NgModel) {
