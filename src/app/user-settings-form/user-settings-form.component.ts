9ff73f996b1f84f3f684ba313c36008b155244af
Hiram Fernandes <hiram.sd@gmail.com>
2020-04-23 11:11:27 +0100

    Added validation information

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -8,19 +9,27 @@ import { UserSettings } from '../data/user-settings';
 })
 export class UserSettingsFormComponent implements OnInit {
 
-  originalUserSettings : UserSettings = {
-    name: 'Milton',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'here are some notes...'
+  originalUserSettings: UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   };
 
-  userSettings : UserSettings = { ...this.originalUserSettings };
-  
+  userSettings: UserSettings = { ...this.originalUserSettings };
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('In onSubmit Event. Valid: ' + form.valid);
+  }
+
+  onBlur(field: NgModel) {
+    console.log('In onBlur Event: field: ' + field.name + ', Valid: ' + field.valid);
   }
 
 }
